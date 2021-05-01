fa789f81b40a13ea294f63dd43efafc713f4cd07
Hongming Li <homgming.li@mail.utoronto.ca>
2021-05-01 17:00:21 -0400

    Update compose file to pull image

---
+++ src/middlewares/inputValidation.ts
@@ -0,0 +1,8 @@
+// import { check } from 'express-validator';
+
+// const isNonEmptyString = (field: string, notStringMessage:string, emptyMessage: string) => {
+//   return check(field).exists()
+//   .trim().escape()
+//   .isString().withMessage(notStringMessage)
+//   .notEmpty().withMessage(emptyMessage);
+// } 
